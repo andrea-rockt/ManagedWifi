e8c78c857b52c0047505acfce0ac73678b9dae53
Andrea Fonti <andreafontict@gmail.com>
2012-07-10 23:14:07 +0200

    Defined Interface IManagedWifiClient

--- IManagedWifiClient.h
+++ IManagedWifiClient.h
@@ -1,3 +1,15 @@
-public interface class IManagedWifiClient{
+#pragma once
 
-};
+#include "Interface.h"
+using namespace System;
+using namespace System::Collections::ObjectModel;
+namespace ManagedWifi {
+	public interface class IManagedWifiClient : IDisposable{
+	public:
+		virtual property ReadOnlyCollection<Interface^> ^ Interfaces{
+			ReadOnlyCollection<Interface^> ^ get();
+		}
+
+		public bool Scan();
+	};
+}
