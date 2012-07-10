af2759117c59d772f3d9c26520fb0dbba0a44af5
Andrea Fonti <andreafontict@gmail.com>
2012-07-11 00:07:13 +0200

    Defined Interface

--- ManagedWifiClient.cpp
+++ ManagedWifiClient.cpp
@@ -1,6 +1,26 @@
 // File DLL principale.
 
 #include "stdafx.h"
-
 #include "ManagedWifiClient.h"
 
+using namespace System::Collections::Generic;
+
+namespace ManagedWifi {
+	
+	ManagedWifiClient::ManagedWifiClient(){
+	}
+	
+	ManagedWifiClient::~ManagedWifiClient(){
+	}
+	
+	ManagedWifiClient::!ManagedWifiClient(){
+	}
+
+	ReadOnlyCollection<Interface^> ^ ManagedWifiClient::Interfaces::get(){
+		return nullptr;
+	}
+
+	IList<Network ^> ^ GetAvailableNetworks(Interface^ i){
+		return nullptr;
+	}
+}
--- ManagedWifiClient.h
+++ ManagedWifiClient.h
@@ -1,13 +1,40 @@
 // ManagedWifi.h
-
 #pragma once
+#include"IManagedWifiClient.h"
+#include "Interface.h"
+#include "Network.h"
 
 using namespace System;
+using namespace System::Collections::Generic;
+
 
 namespace ManagedWifi {
 
-	public ref class ManagedWifiClient
+	public ref class ManagedWifiClient : public IManagedWifiClient
 	{
+	public:
+		ManagedWifiClient();
+		
+		~ManagedWifiClient();
+		
+		virtual property ReadOnlyCollection<Interface^> ^ Interfaces{
+			ReadOnlyCollection<Interface^> ^ get(void);
+		};
+
+		virtual bool Scan();
+
+		property bool IsDisposed{
+			bool get();
+		};
+
+	private:
+		bool _isDisposed;
+
+	protected:
+		!ManagedWifiClient();
 		// TODO: aggiungere qui i metodi per la classe.
+
+	internal:
+		IList<Network ^> ^ GetAvailableNetworks(Interface^ i);
 	};
 }
