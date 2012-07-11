1c4c355b87a21dbb358bfde51ea7e1a1319aa447
Andrea Fonti <andreafontict@gmail.com>
2012-07-11 11:57:36 +0200

    Trivial test

--- ManagedWifiTest/Program.cs
+++ ManagedWifiTest/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using ManagedWifi;
 
 namespace ManagedWifiTest
 {
@@ -9,6 +10,18 @@ namespace ManagedWifiTest
     {
         static void Main(string[] args)
         {
+            using (var context = new ManagedWifiContext(ManagedWifiContext.NWlanVersion.WindowsXP))
+            {
+                var a = context.IsDisposed;
+
+                foreach (var iface in context.Interfaces)
+                {
+                        Console.WriteLine("Interface:");
+                        Console.WriteLine("\tDescription: {0}",iface.Description);
+                        Console.WriteLine("\tState: {0}",iface.State.ToString());
+                        Console.WriteLine("\tGUID: {0}",iface.Guid.ToString());
+                }
+            }
         }
     }
 }
