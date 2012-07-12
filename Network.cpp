06a430e3e7a2a9a65cb38ae0a3373447d8e1f8db
Andrea Fonti <andreafontict@gmail.com>
2012-07-12 12:10:44 +0200

    Implemented Network

--- Network.cpp
+++ Network.cpp
@@ -2,7 +2,27 @@
 #include "Network.h"
 
 namespace ManagedWifi{
-Network::Network(void)
-{
-}
+	Network::Network(BSSType type, String ^ ssid, IList<PhysicalAddress^> ^ bssids, ULONG signal_strength)
+			{
+				this->_type=type;
+				this->_ssid=ssid;
+				this->_bssids=bssids;
+				this->_signalStrength=signal_strength;
+			}
+
+			Network::BSSType Network::Type::get(){
+				return this->_type;
+			}
+
+			String ^ Network::SSID::get(){
+				return this->_ssid;
+			}
+
+			ReadOnlyCollection<PhysicalAddress^> ^ Network::BSSIDs::get(){
+				return gcnew ReadOnlyCollection<PhysicalAddress^>(this->_bssids);
+			}
+
+			ULONG Network::SignalStrength::get(){
+				return this->_signalStrength;
+			}
 }
--- Network.h
+++ Network.h
@@ -1,8 +1,35 @@
 #pragma once
+
+using namespace System;
+using namespace System::Collections::Generic;
+using namespace System::Collections::ObjectModel;
+using namespace System::Net::NetworkInformation;
+
 namespace ManagedWifi{
-public ref class Network
-{
-public:
-	Network(void);
-};
+	public ref class Network
+	{
+	public:
+		enum class BSSType{
+			Infrastructure=1,
+			Independent=2,
+			Any=3
+		};
+
+
+		property BSSType Type{BSSType get();};
+
+		property String ^ SSID{String ^ get();};
+
+		property ReadOnlyCollection<PhysicalAddress^> ^ BSSIDs{ReadOnlyCollection<PhysicalAddress^> ^ get();};
+
+		property ULONG SignalStrength {ULONG get();};
+		
+		Network(BSSType type, String ^ ssid, IList<PhysicalAddress^> ^ bssids, ULONG signal_strength);
+
+	private:
+		initonly BSSType _type;
+		initonly String ^ _ssid;
+		initonly IList<PhysicalAddress^>^ _bssids;
+		initonly ULONG _signalStrength;
+	};
 }
